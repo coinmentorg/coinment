1bbee9822dc10679333e87da4e3ecdc74b0caaf8
zkinc <zkinc@zkincdeMBP.lan>
2018-02-04 20:38:57 +0800

    base pages

---
+++ src/components/rank/Hot.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="hot-rank">
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag common">中等</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag bad">很差！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag bad">很差！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag bad">很差！</span>
+    </div>
+    <div class="rank-item">
+      <img src="../../assets/img/logo-vertical.png" class="logo" alt="logo">
+      <div class="inner-wrap">
+        <p class="rank-name">Coinment.org</p>
+        <p class="rank-num">171,246 条有效点评</p>
+      </div>
+      <span class="tag good">很好！</span>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: "hot-rank",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+#hot-rank{
+  .rank-item{
+    height: 60px;
+    padding:0 60px;
+    position: relative;
+    cursor: pointer;
+    border-bottom: 1px solid #ddd;
+    &:hover{
+      background-color: #f4f4f4;
+    }
+    &:active{
+      background-color: darken(#f4f4f4, 5%);
+    }
+    .logo{
+      height: 40px;
+      width: 40px;
+      top:10px;
+      left:10px;
+      position: absolute;
+      border-radius: 50%;
+      box-sizing: border-box;
+      border: 1px solid #ddd;
+    }
+    .inner-wrap{
+      padding: 10px 0;
+      font-size: 12px;
+      p{
+        height: 20px;
+        line-height: 20px;
+        &.rank-name{
+          color: #555;
+        }
+        &.rank-num{
+          color: #bbb;
+        }
+      }
+    }
+    .tag{
+      font-size: 15px;
+      font-weight: bold;
+      position: absolute;
+      right: 0;
+      top:0;
+      width: 60px;
+      line-height: 60px;
+      &.good{
+        color: #42CB55;
+      }
+      &.common{
+        color: #FBBE46;
+      }
+      &.bad{
+        color: #FD7975;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/rank/Rank.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="rank-wrap">
+    <div class="rank-nav clearfix">
+      <router-link class="left" :to="{ name: 'NewRank'}">最新点评</router-link>
+      <router-link class="center" :to="{ name: 'HotRank'}">热门站点</router-link>
+      <router-link class="right" :to="{ name: 'HighRank'}">高额奖励</router-link>
+    </div>
+    <div class="rank-content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rank',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.rank-wrap{
+  padding-top: 38px;
+  height: 100%;
+  box-sizing: border-box;
+  position: relative;
+  .rank-nav{
+    top:0;
+    left:0;
+    height: 38px;
+    width: 100%;
+    text-align: center;
+    line-height: 38px;
+    background-color: #555;
+    position: absolute;
+    color:#fff;
+    font-size: 12px;
+    a{
+      text-decoration: none;
+      &.router-link-active{
+        font-size: 14px;
+        font-weight: bolder;
+      }
+      &.left{
+        float: left;
+        padding-left: 10px;
+      }
+      &.right{
+        float: right;
+        padding-right: 10px;
+      }
+    }
+  }
+  .rank-content{
+    height: 100%;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+        width: 4px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #ddd;
+      border-radius: 2px;
+    }
+    &::-webkit-scrollbar-track {
+      border:1px solid #eee;
+    }
+  }
+}
+</style>
